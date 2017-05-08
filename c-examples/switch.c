db362e0ff3db820188230864140f791625f134ad
Linda Ortega Cordoves <linda.ortega2013@gmail.com>
2017-05-08 17:02:20 -0400

    resolved merge conflict

--- c-examples/switch.c
+++ c-examples/switch.c
@@ -2,17 +2,22 @@
 
 
 int main() {
-  int a;
-  a = 1;
-  switch(a) {
-  case -1:
-    printf("a is -1\n");
-  case 0:
-    printf("a is 0\n");
-  case 1:
-    printf("a is 1\n");
-  default:
-    printf("default a\n");
+  int b = 0;
+
+  while (b == 0) { 
+    int a;
+    a = 1;
+    switch(a) {
+    case -1:
+        printf("a is -1\n");
+    case 0:
+        printf("a is 0\n");
+    case 1:
+        printf("a is 1\n");
+    default:
+        b = 1;
+        printf("default a\n");
+    }
   }
   return 0;
 }
