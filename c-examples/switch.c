f00ed39438424d4966db4573819ea6280f14f428
mesamason <mbl2109@columbia.edu>
2017-05-09 01:28:21 -0400

    got receive functions pulling arguments with no errors, need to actually test it

--- c-examples/switch.c
+++ c-examples/switch.c
@@ -1,24 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+struct foo {
+  int a;
+};
 
-
+struct bar {
+  int a;
+  int b;
+};
+  
 int main() {
   int b = 0;
+
+
 
   while (b == 0) { 
     printf("entered while loop\n");
     int case_n = -1;
     int a = 5;
-
+    void *var = malloc(sizeof(struct bar));
+    struct bar *castBar;
+    struct foo *castFoo;
     switch(case_n) {
     case -1:
-        printf("-1: case_n is %d\n",case_n);
+      castFoo = (struct foo *)var;
+      castFoo->a = 42;
+      printf("-1: case_n castedFoo is %d\n",castFoo->a);
+      break;
     case 0:
-        a++;
-        printf("0: case_n is %d\n", case_n);
+      a++;
+      printf("0: case_n castedBar is %d\n", case_n);
+      break;
     case 1:
-        printf("1: case_n is %d\n", case_n);
+      printf("1: case_n is %d\n", case_n);
+      break;
     default:
-        printf("default case_n is %d\n", case_n);
+      printf("default case_n is %d\n", case_n);
+      break;
     }
 
     case_n = case_n + 1;
